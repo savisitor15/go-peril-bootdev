f2c74a1ef45e4aaaf7480273f25a56b0679c8a73
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2025-10-28 20:46:59 +0200

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	fmt.Println("Starting Peril server...")
+	rabbitHost := "amqp://guest:guest@localhost:5672/"
+	rabbitConn, err := amqp.Dial(rabbitHost)
+	if err != nil {
+		fmt.Println(fmt.Errorf("error creating connetion: %w", err))
+	}
+	defer rabbitConn.Close()
+	fmt.Printf("connection esstablished to %s \n", rabbitHost)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool, 1)
+	go func() {
+
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+
+}
